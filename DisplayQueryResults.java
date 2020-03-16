9ff57c091800cec4dfd65406d92c3631e7c505d6
Sam Norain <62264157+samtheengineer@users.noreply.github.com>
2020-03-16 16:44:36 -0400

    Update DisplayQueryResults.java

--- DisplayQueryResults.java
+++ DisplayQueryResults.java
@@ -1,6 +1,5 @@
 package books;
 
-// Fig. X: DisplayQueryResults.java
 // Main application class that loads and displays the GUI.
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -24,18 +23,3 @@ public class DisplayQueryResults extends Application {
       launch(args);
    }
 }
-
-/**************************************************************************
- * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
